b59a47e47271258fc1fb11c93a14f8ba8e70318d
masoud akbari <masoudxxaa02@gmail.com>
2025-08-31 15:01:32 +0330

    First commit - setup project with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dana: ["Dana", "sans-serif"],
+        morabba: ["Morabba", "sans-serif"],
+      },
+    },
+  },
+};
