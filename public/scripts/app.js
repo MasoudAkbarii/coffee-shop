61298f690c0f987f59da8443cc0cc25b768a82b7
Masoud Akabari <masoudxxaa02@gmail.com>
2025-09-08 18:03:03 +0330

    compelet a part of mobile menu and coding some js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,26 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click", () => {
-  if (localStorage.theme === "dark") {
-    document.documentElement.classList.remove("dark");
-    localStorage.theme = "light";
-  } else {
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-  }
+const toggleThemeBtns = document.querySelectorAll('.toggle-theme');
+const submenuOpenBtn = document.querySelector('.submenu-open-btn');
+const submenu = document.querySelector('.submenu');
+const navOpenBtn = document.querySelector('.nav-icon');
+
+const nav = document.querySelector('.nav');
+toggleThemeBtns.forEach(btn => {
+  btn.addEventListener('click', function () {
+    if (localStorage.theme === 'dark') {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = 'light';
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  });
+});
+
+submenuOpenBtn.addEventListener('click', e => {
+  e.currentTarget.parentElement.classList.toggle('text-orange-300');
+  submenu.classList.toggle('submenu--open');
+});
+navOpenBtn.addEventListener('click', () => {
+  nav.classList.remove('-right-64');
+  nav.classList.add('right-0');
 });
